2836abeecd6f440cdf75ffa4e622822c3e7a9d19
ottonove <niigata01web@gmail.com>
2019-09-25 09:40:03 +0900

    Initial Commit

---
+++ Tokoi/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Tokoi
+//
+//  Created by ottomac on 2019/09/25.
+//  Copyright © 2019年 ottomac. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
